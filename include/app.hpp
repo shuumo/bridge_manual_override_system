e575a37ec5432b8865d2e9d62478a1fa8d305bfd
arnav <78186202+shuumo@users.noreply.github.com>
2025-10-28 22:53:17 +1100

    Update app.hpp

--- include/app.hpp
+++ include/app.hpp
@@ -1,7 +1,5 @@
 /* PURPOSE:
- * wrapper that puts the other shit together because I am in a rush lol.
- * Also it is easier to delegate shit to chatgpt this way so it doesn't break
- * stuff cos it can be dumb as fuck at c++ ngl.
+ * wrapper that puts the other shit together.
 */
 
 #pragma once
